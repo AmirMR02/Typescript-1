e56661a4ac2d0310fa1bf369140c7d06e8fc7c5a
amirMR02 <a.m.rashvand02@gmail.com>
2024-01-21 14:34:51 +0330

    objects commited

---
+++ objects.js
@@ -0,0 +1,2 @@
+//creating a  simple obejct in ts
+var person = { name: "Amirmohammad", age: 22 };
---
+++ objects.ts
@@ -0,0 +1,2 @@
+//creating a  simple obejct in ts
+let person: { name: string; age: number } = { name: "Amirmohammad", age: 22 };
