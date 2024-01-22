9cb08e413bec5648d3ef0220ad9b926602adb533
amirMR02 <a.m.rashvand02@gmail.com>
2024-01-22 14:32:11 +0330

    still working on objects

--- objects.ts
+++ objects.ts
@@ -1,2 +1,9 @@
 //creating a  simple obejct in ts
 let person: { name: string; age: number } = { name: "Amirmohammad", age: 22 };
+
+//passsing object as function parameter
+function singerMan(person: { name: string; age: number }) {
+  console.log(`Hi ${person.name}, i know you are a singer`);
+}
+
+singerMan({ name: "amirM", age: 22 });
