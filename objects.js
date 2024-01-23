5b35051b4a266c15733118062bb9cf0fa7a49ec7
amirMR02 <a.m.rashvand02@gmail.com>
2024-01-23 14:40:33 +0330

    still working with objects

--- objects.js
+++ objects.js
@@ -1,2 +1,13 @@
 //creating a  simple obejct in ts
 var person = { name: "Amirmohammad", age: 22 };
+//passsing object as function parameter
+function singerMan(person) {
+    console.log("Hi ".concat(person.name, ", i know you are a singer"));
+}
+singerMan({ name: "amirM", age: 22 });
+//type is method that we can create any type with it!
+var anotherHuman = {
+    name: "amirmohammad",
+    age: 22,
+    job: "student",
+};
--- objects.ts
+++ objects.ts
@@ -7,3 +7,17 @@ function singerMan(person: { name: string; age: number }) {
 }
 
 singerMan({ name: "amirM", age: 22 });
+
+//creating a simple TYPE
+type Human = {
+  name: string;
+  age: number;
+  job: string;
+};
+//type is method that we can create any type with it!
+
+const anotherHuman: Human = {
+  name: "amirmohammad",
+  age: 22,
+  job: "student",
+};
