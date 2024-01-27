140366f923df83a363b779eefd64a4a002cf6bb2
amirMR02 <a.m.rashvand02@gmail.com>
2024-01-27 23:36:00 +0330

    working with objects

---
+++ arrays.js
@@ -0,0 +1,5 @@
+//a simple array definition in typesript
+//in this example we define a array with some value(it can have pop and push)
+var threeNumber = [1, 2, 3];
+threeNumber.push(4);
+threeNumber.pop();
---
+++ arrays.ts
@@ -0,0 +1,6 @@
+//a simple array definition in typesript
+//in this example we define a array with some value(it can have pop and push)
+const threeNumber: number[] = [1, 2, 3];
+
+threeNumber.push(4);
+threeNumber.pop();
