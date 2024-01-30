b86aad5711fcb660e32ef8a57a15fe3975492896
amirMR02 <a.m.rashvand02@gmail.com>
2024-01-30 22:59:31 +0330

    last work on arrays

--- arrays.js
+++ arrays.js
@@ -7,3 +7,33 @@ threeNumber.pop();
 var threeNumbewr2 = [1, 2, 3];
 var thisNumbers = [];
 thisNumbers.push({ x: 1, y: 4 });
+//EXERCISE
+// **********************************************
+// ******************* PART 1 *******************
+// **********************************************
+// Create an empty array of numbers called "age":
+//Asnwer
+var age = [];
+// **********************************************
+// ******************* PART 2 *******************
+// **********************************************
+// Create an array variable called gameBoard that starts as an empty array.
+// It should be typed to hold a 2 dimensional array of strings
+//Answer
+var gamesTable = [[]];
+var testPr = [];
+testPr.push({ name: "coffe mug", price: 11.5 });
+// **********************************************
+// ******************* PART 4 *******************
+// **********************************************
+// Write a function called getAll that accepts an array of Product types
+// It should return the sum of all the products' prices
+function getAll(p) {
+    return p.reduce(function (acc, i) { return acc + i.price; }, 0);
+}
+var n = getAll([
+    { name: "A", price: 20 },
+    { name: "A", price: 20 },
+    { name: "A", price: 60 },
+]);
+console.log(n); //will be 100
--- arrays.ts
+++ arrays.ts
@@ -16,3 +16,46 @@ type thisType = {
 
 let thisNumbers: Array<thisType> = [];
 thisNumbers.push({ x: 1, y: 4 });
+
+//EXERCISE
+// **********************************************
+// ******************* PART 1 *******************
+// **********************************************
+// Create an empty array of numbers called "age":
+//Asnwer
+const age: number[] = [];
+// **********************************************
+// ******************* PART 2 *******************
+// **********************************************
+// Create an array variable called gameBoard that starts as an empty array.
+// It should be typed to hold a 2 dimensional array of strings
+//Answer
+const gamesTable: string[][] = [[]];
+// **********************************************
+// ******************* PART 3 *******************
+// **********************************************
+// Create a Products type that contains a name and a price.
+// An example product could be:
+// {name: "coffee mug", price: 11.50}
+type products = {
+  name: string;
+  price: number;
+};
+const testPr: Array<products> = [];
+testPr.push({ name: "coffe mug", price: 11.5 });
+
+// **********************************************
+// ******************* PART 4 *******************
+// **********************************************
+// Write a function called getAll that accepts an array of Product types
+// It should return the sum of all the products' prices
+
+function getAll(p: Array<products>) {
+  return p.reduce((acc, i) => acc + i.price, 0);
+}
+const n = getAll([
+  { name: "A", price: 20 },
+  { name: "A", price: 20 },
+  { name: "A", price: 60 },
+]);
+console.log(n); //will be 100
