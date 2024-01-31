76d067a14e0af7709a761c0780ceec7683d08426
amirMR02 <a.m.rashvand02@gmail.com>
2024-01-31 15:57:12 +0330

    start working with union types

---
+++ unionTypes.js
@@ -0,0 +1,4 @@
+//union type is a type that can be two or more type
+var twoT;
+twoT = 2; //accept
+twoT = "2"; //accept
---
+++ unionTypes.ts
@@ -0,0 +1,4 @@
+//union type is a type that can be two or more type
+let twoT: string | number;
+twoT = 2; //accept
+twoT = "2"; //accept
