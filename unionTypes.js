73a037d58318631588dc37a58cdc80826ff1f5a7
amirMR02 <a.m.rashvand02@gmail.com>
2024-02-01 15:46:37 +0330

    still working with unionTypes

--- unionTypes.js
+++ unionTypes.js
@@ -2,3 +2,7 @@
 var twoT;
 twoT = 2; //accept
 twoT = "2"; //accept
+//it can 2 property of each object
+var cp;
+cp = { x: 2, y: 3 };
+cp = { lat: 2312, long: 344 };
--- unionTypes.ts
+++ unionTypes.ts
@@ -2,3 +2,20 @@
 let twoT: string | number;
 twoT = 2; //accept
 twoT = "2"; //accept
+
+//another example with combineing two type togather
+type coord = {
+  lat: number;
+  long: number;
+};
+
+type points = {
+  x: number;
+  y: number;
+};
+
+//it can 2 property of each object
+let cp: coord | points;
+
+cp = { x: 2, y: 3 };
+cp = { lat: 2312, long: 344 };
