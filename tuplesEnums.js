f6c4ca15d1b917181c88260ac3256db2a1536cd7
amirMR02 <a.m.rashvand02@gmail.com>
2024-02-07 22:36:49 +0330

    still working on tuples and enums

--- tuplesEnums.js
+++ tuplesEnums.js
@@ -1,2 +1,3 @@
 //with tuples we can define constant length
 var RGB = [1, 2, 3];
+var TestHttpResponse = [200, "ok"];
--- tuplesEnums.ts
+++ tuplesEnums.ts
@@ -1,2 +1,4 @@
 //with tuples we can define constant length
 const RGB: [number, number, number] = [1, 2, 3];
+
+const TestHttpResponse: [number, string] = [200, "ok"];
