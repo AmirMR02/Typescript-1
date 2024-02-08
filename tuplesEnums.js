53291a01b3b06bf61f48170a43916a49ac79e8d9
amirMR02 <a.m.rashvand02@gmail.com>
2024-02-08 16:37:15 +0330

    still working with eums and tuples

--- tuplesEnums.js
+++ tuplesEnums.js
@@ -1,3 +1,9 @@
 //with tuples we can define constant length
 var RGB = [1, 2, 3];
 var TestHttpResponse = [200, "ok"];
+//enums will help usto create constant values and use them all the time
+var tooMuchName;
+(function (tooMuchName) {
+    tooMuchName[tooMuchName["muNymber"] = 1234568479846] = "muNymber";
+})(tooMuchName || (tooMuchName = {}));
+var a = tooMuchName.muNymber;
--- tuplesEnums.ts
+++ tuplesEnums.ts
@@ -2,3 +2,10 @@
 const RGB: [number, number, number] = [1, 2, 3];
 
 const TestHttpResponse: [number, string] = [200, "ok"];
+
+//enums will help usto create constant values and use them all the time
+enum tooMuchName {
+  muNymber = 1234568479846,
+}
+
+const a = tooMuchName.muNymber;
