98860fc21412177edbc2c80cce879437fa54a0a4
amirMR02 <a.m.rashvand02@gmail.com>
2024-02-19 18:32:15 +0330

    still working with classes

--- classes.js
+++ classes.js
@@ -4,3 +4,20 @@ class ImClass {
   p1;
   p2;
 }
+
+//qnother goof example in js world
+class Home {
+  meter;
+  doors;
+  windows;
+  rooms;
+  floor;
+}
+
+//let's make a home
+const myHome = new Home();
+myHome.meter = 75;
+myHome.doors = 1;
+myHome.windows = 4;
+myHome.rooms = 2;
+myHome.floor = 1;
