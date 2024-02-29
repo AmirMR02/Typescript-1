5855a45bfcba58f9ff32b22dc6db0585f2301c8e
amirMR02 <a.m.rashvand02@gmail.com>
2024-02-29 22:13:27 +0330

    start working with generic

---
+++ genericClasses.ts
@@ -0,0 +1,5 @@
+//what is Generic?
+//a feature that allow us write reusable function
+//in a simple word: we can use a function in multipile way in mutipile situation
+//an simple function that we saw before :
+const OldG: Array<number> = [1, 2, 3];
