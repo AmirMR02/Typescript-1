08dbe8ee76bb00a9abc08ecaafabb94df6235044
amirMR02 <a.m.rashvand02@gmail.com>
2024-03-04 22:49:17 +0330

    type narrowing

---
+++ TypeNarrowing.ts
@@ -0,0 +1,23 @@
+//type narrowing means "check the type and then do something related woth that type"
+//consider this code
+// function TestSum(a : number | string , b : number) {
+//     return a + b; //ERROR
+// }
+
+//in this example we have Error because of type "a"
+//the compiler can't undrestand the type and
+//typescript is confused to what to do!
+
+//so we do this
+
+function TestSum(a: number | string, b: number) {
+  //we use 'typeof' gaurd for narrowing!
+  if (typeof a === "number") {
+    return a + b;
+  } else {
+    return `${b} ${a}`;
+  }
+}
+
+TestSum(10, 12); //accept
+TestSum("ball", 12); //accept
